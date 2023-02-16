d4f152980250948b181c9323320e6bd571d142b5
Jimmykoki <jianlil2@uci.edu>
2023-02-16 08:09:33 -0800

    Configure react-route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './App.module.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages';
+import { HomePage, SignInPage, RegisterPage, DetailPage } from './pages';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +9,10 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="*" element={<h1>404: Not Found</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
