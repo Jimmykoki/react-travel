8c8149cb8fc4c2a383d1566e1e9d1d4fe71173ef
Jimmykoki <jianlil2@uci.edu>
2023-03-22 00:37:53 -0700

    Use jwt to grab data and implement Private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './App.module.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage, SignInPage, RegisterPage, DetailPage } from './pages';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  HomePage,
+  SignInPage,
+  RegisterPage,
+  DetailPage,
+  SearchPage,
+  ShoppingCartPage,
+  PlaceOrderPage,
+} from './pages';
+import { useSelector, useAppDispatch } from './redux/hook';
+import { getShoppingCart } from './redux/shoppingCart/slice';
 
 const App: React.FC = () => {
+  const jwt = useSelector((state) => state.user.token);
+  const PrivateRoutes = ({ children }) => {
+    if (jwt) {
+      return children;
+    } else {
+      return <Navigate to="/signin" />;
+    }
+  };
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (jwt) {
+      dispatch(getShoppingCart(jwt));
+    }
+  }, [jwt]);
+
   return (
     <div className={styles.App}>
       <BrowserRouter>
@@ -12,6 +39,23 @@ const App: React.FC = () => {
           <Route path="/signin" element={<SignInPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="/search/:keywords" element={<SearchPage />} />
+          <Route
+            path="/shoppingCart"
+            element={
+              <PrivateRoutes>
+                <ShoppingCartPage />
+              </PrivateRoutes>
+            }
+          />
+          <Route
+            path="/placeOrder"
+            element={
+              <PrivateRoutes>
+                <PlaceOrderPage />
+              </PrivateRoutes>
+            }
+          />
           <Route path="*" element={<h1>404: Not Found</h1>} />
         </Routes>
       </BrowserRouter>
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import './i18n/configs';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import rootStore from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={rootStore.store}>
+      <PersistGate persistor={rootStore.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
