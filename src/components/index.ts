d811e7fe19db81341dd8f1f01202dd623c301ad9
Jimmykoki <jianlil2@uci.edu>
2023-03-22 00:57:20 -0700

    Updata exported component data

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,8 @@ export * from './productCollection';
 export * from './businessPartner';
 export * from './productIntro';
 export * from './productComments';
+export * from './filter';
+export * from './productList';
+export * from './paymentCard';
+export * from './paymentForm';
+export * from './checkOutCard';
