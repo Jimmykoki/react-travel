c35a5f7253f91e2ff7e97a75e6eff7293dcd54f1
Jimmykoki <jianlil2@uci.edu>
2023-03-22 01:05:31 -0700

    Update and fixed pages

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import {
-  Header,
-  Footer,
-  ProductIntro,
-  ProductComments,
-} from '../../components';
+import { ProductIntro, ProductComments } from '../../components';
 import styles from './DetailPage.module.css';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import {
   Col,
@@ -17,8 +13,16 @@ import {
   Typography,
   Anchor,
   Menu,
+  Button,
 } from 'antd';
 import { commentMockData } from './mockup';
+import {
+  productDetailSlice,
+  getProductDetail,
+} from '../../redux/productDetail/slice';
+import { useSelector, useAppDispatch } from '../../redux/hook';
+import { MainLayout } from '../../layouts/mainLayout';
+import { addShoppingCartItem } from '../../redux/shoppingCart/slice';
 
 type MatchParams = {
   id: string;
@@ -26,26 +30,26 @@ type MatchParams = {
 
 export const DetailPage: React.FC = () => {
   const { id } = useParams<MatchParams>(); // grab the id from the url
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const [product, setProduct] = useState<any>([]);
+  // const [loading, setLoading] = useState<boolean>(true);
+  // const [error, setError] = useState<string | null>(null);
+  // const [product, setProduct] = useState<any>([]);
   const { RangePicker } = DatePicker;
 
+  const loading = useSelector((state) => state.productDetail.loading);
+  const error = useSelector((state) => state.productDetail.error);
+  const product = useSelector((state) => state.productDetail.data);
+
+  const dispatch = useAppDispatch();
+
+  const jwt = useSelector((state) => state.user.token) as string;
+  const shoppingCartLoading = useSelector(
+    (state) => state.shoppingCart.loading
+  );
+
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const { data } = await axios.get(
-          `http://123.56.149.216:8089/api/touristRoutes/${id}`
-        );
-        setProduct(data);
-        setLoading(false);
-      } catch (error) {
-        setError(error instanceof Error ? error.message : 'error');
-        setLoading(false);
-      }
-    };
-    fetchData();
+    if (id) {
+      dispatch(getProductDetail(id));
+    }
   }, []);
 
   if (loading) {
@@ -67,81 +71,92 @@ export const DetailPage: React.FC = () => {
   }
 
   return (
-    <>
-      <Header />
-      <div className={styles['page-content']}>
-        <div className={styles['product-intro-container']}>
-          <Row>
-            <Col span={13}>
-              <ProductIntro
-                title={product.title}
-                shortDescription={product.description}
-                price={product.originalPrice}
-                coupons={product.coupons}
-                points={product.points}
-                discount={product.discount}
-                rating={product.rating}
-                pictures={product.touristRoutePictures.map((p: any) => p.url)}
-              />
-            </Col>
-            <Col span={11}>
-              <RangePicker open style={{ marginTop: 20 }} />
-            </Col>
-          </Row>
-        </div>
+    <MainLayout>
+      <div className={styles['product-intro-container']}>
+        <Row>
+          <Col span={13}>
+            <ProductIntro
+              title={product.title}
+              shortDescription={product.description}
+              price={product.originalPrice}
+              coupons={product.coupons}
+              points={product.points}
+              discount={product.discount}
+              rating={product.rating}
+              pictures={product.touristRoutePictures.map((p: any) => p.url)}
+            />
+          </Col>
+          <Col span={11}>
+            <Button
+              style={{ marginTop: 50, marginBottom: 30, display: 'block' }}
+              type="primary"
+              danger
+              loading={shoppingCartLoading}
+              onClick={() => {
+                dispatch(
+                  addShoppingCartItem({ jwt, touristRouteId: product.id })
+                );
+              }}
+            >
+              <ShoppingCartOutlined />
+              加入购物车
+            </Button>
+            <RangePicker open style={{ marginTop: 20 }} />
+          </Col>
+        </Row>
+      </div>
 
-        <Anchor className={styles['product-detail-anchor']}>
-          <Menu mode="horizontal">
-            <Menu.Item key="1">
-              <Anchor.Link href="#feature" title="产品特色"></Anchor.Link>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Anchor.Link href="#fees" title="费用"></Anchor.Link>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <Anchor.Link href="#notes" title="预定须知"></Anchor.Link>
-            </Menu.Item>
-            <Menu.Item key="5">
-              <Anchor.Link href="#comments" title="用户评价"></Anchor.Link>
-            </Menu.Item>
-          </Menu>
-        </Anchor>
-        {/** Product introduction Module */}
-        <div id="feature" className={styles['product-detail-container']}>
-          <Divider orientation="center">
-            <Typography.Title level={3}>产品特色</Typography.Title>
-          </Divider>
-          <div dangerouslySetInnerHTML={{ __html: product.features }}></div>
-        </div>
+      <Anchor className={styles['product-detail-anchor']}>
+        <Menu mode="horizontal">
+          <Menu.Item key="1">
+            <Anchor.Link href="#feature" title="产品特色"></Anchor.Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Anchor.Link href="#fees" title="费用"></Anchor.Link>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Anchor.Link href="#notes" title="预定须知"></Anchor.Link>
+          </Menu.Item>
+          <Menu.Item key="5">
+            <Anchor.Link href="#comments" title="用户评价"></Anchor.Link>
+          </Menu.Item>
+        </Menu>
+      </Anchor>
 
-        {/** Tour expense*/}
-        <div id="fees" className={styles['product-detail-container']}>
-          <Divider orientation="center">
-            <Typography.Title level={3}>旅行费用</Typography.Title>
-          </Divider>
-          <div dangerouslySetInnerHTML={{ __html: product.fees }}></div>
-        </div>
+      {/** Product introduction Module */}
+      <div id="feature" className={styles['product-detail-container']}>
+        <Divider orientation="center">
+          <Typography.Title level={3}>产品特色</Typography.Title>
+        </Divider>
+        <div dangerouslySetInnerHTML={{ __html: product.features }}></div>
+      </div>
 
-        {/** Relatived attention */}
-        <div id="notes" className={styles['product-detail-container']}>
-          <Divider orientation="center">
-            <Typography.Title level={3}>旅行须知</Typography.Title>
-          </Divider>
+      {/** Tour expense*/}
+      <div id="fees" className={styles['product-detail-container']}>
+        <Divider orientation="center">
+          <Typography.Title level={3}>旅行费用</Typography.Title>
+        </Divider>
+        <div dangerouslySetInnerHTML={{ __html: product.fees }}></div>
+      </div>
 
-          <div dangerouslySetInnerHTML={{ __html: product.notes }}></div>
-        </div>
+      {/** Relatived attention */}
+      <div id="notes" className={styles['product-detail-container']}>
+        <Divider orientation="center">
+          <Typography.Title level={3}>旅行须知</Typography.Title>
+        </Divider>
 
-        {/** Users comments*/}
-        <div id="comments" className={styles['product-detail-container']}>
-          <Divider orientation="center">
-            <Typography.Title level={3}>用户评价</Typography.Title>
-          </Divider>
-          <div style={{ margin: 40 }}>
-            <ProductComments data={commentMockData} />
-          </div>
+        <div dangerouslySetInnerHTML={{ __html: product.notes }}></div>
+      </div>
+
+      {/** Users comments*/}
+      <div id="comments" className={styles['product-detail-container']}>
+        <Divider orientation="center">
+          <Typography.Title level={3}>用户评价</Typography.Title>
+        </Divider>
+        <div style={{ margin: 40 }}>
+          <ProductComments data={commentMockData} />
         </div>
       </div>
-      <Footer />
-    </>
+    </MainLayout>
   );
 };
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -13,6 +13,7 @@ import { withTranslation, WithTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { getRecommendRoductsListActionCreator } from '../../redux/recommendProduts/recommendProductsActions';
+import { MainLayout } from '../../layouts/mainLayout';
 import sideImage from '../../assets/images/side-image.png';
 import sideImage2 from '../../assets/images/side-image-2.png';
 import sideImage3 from '../../assets/images/side-image-3.png';
@@ -65,50 +66,44 @@ class HomePageComponent extends React.Component<PropsType> {
     }
 
     return (
-      <>
-        <Header />
-        {/* Content */}
-        <div className={styles['page-content']}>
-          <Row style={{ marginTop: 20 }}>
-            <Col span={6}>
-              <SideMenu />
-            </Col>
-            <Col span={18}>
-              <Carousel />
-            </Col>
-          </Row>
-          <ProductCollection
-            title={
-              <Typography.Title level={3} type="warning">
-                {t('home_page.hot_recommended')}
-              </Typography.Title>
-            }
-            sideImage={sideImage}
-            products={productList[0].touristRoutes}
-          />
-          <ProductCollection
-            title={
-              <Typography.Title level={3} type="danger">
-                {t('home_page.new_arrival')}
-              </Typography.Title>
-            }
-            sideImage={sideImage2}
-            products={productList[1].touristRoutes}
-          />
-          <ProductCollection
-            title={
-              <Typography.Title level={3} type="success">
-                {t('home_page.domestic_travel')}
-              </Typography.Title>
-            }
-            sideImage={sideImage3}
-            products={productList[2].touristRoutes}
-          />
-          <BusinessPartner />
-        </div>
-
-        <Footer />
-      </>
+      <MainLayout>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <SideMenu />
+          </Col>
+          <Col span={18}>
+            <Carousel />
+          </Col>
+        </Row>
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="warning">
+              {t('home_page.hot_recommended')}
+            </Typography.Title>
+          }
+          sideImage={sideImage}
+          products={productList[0].touristRoutes}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="danger">
+              {t('home_page.new_arrival')}
+            </Typography.Title>
+          }
+          sideImage={sideImage2}
+          products={productList[1].touristRoutes}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="success">
+              {t('home_page.domestic_travel')}
+            </Typography.Title>
+          }
+          sideImage={sideImage3}
+          products={productList[2].touristRoutes}
+        />
+        <BusinessPartner />
+      </MainLayout>
     );
   }
 }
